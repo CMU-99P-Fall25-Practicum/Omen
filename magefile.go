2dbfa2fb4b89561de146e88849c3aa7c1d366541
R Landau <rflandau@pm.me>
2025-10-04 10:17:22 -0700

    update magefile to build all binaries

--- magefile.go
+++ magefile.go
@@ -5,17 +5,34 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"io/fs"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
 )
 
-const buildDir string = "artefacts"
+const (
+	buildDir string = "artefacts"
+
+	coordinatorBin   string = "coordinator"
+	spawnTopoBin     string = "1_spawn"
+	outputProcessBin string = "2_output_processing"
+)
 
 var Default = Build
+
+//#region module building
+
+// BuildCoordinator generates the coordinator binary and sits it in ./artefacts/
+func BuildCoordinator() error {
+	mg.Deps(artefactDirectoryExists)
+	_, err := sh.Exec(nil, nil, nil, "go", "build", "-o", "artefacts/"+coordinatorBin, "./coordinator")
+	return err
+}
 
 // DockerizeIV recompiles the input validation docker container.
 func DockerizeIV() error {
@@ -23,16 +40,42 @@ func DockerizeIV() error {
 	return sh.Run("docker", "build", "-t", "omen-input-validator", "modules/0_input/")
 }
 
-// ensures Docker is in path
-func dockerInPath() error {
-	_, err := exec.LookPath("docker")
+// BuildSpawnTopo builds the binary for the glue module.
+func BuildSpawnTopo() error {
+	mg.Deps(artefactDirectoryExists)
+	return sh.Run("go", "build", "-C", "modules/1_spawn_topology/", "-o", "../../artefacts/"+spawnTopoBin)
+}
+
+// BuildOutputProcessing builds the binary for the output coalesce module.
+func BuildOutputProcessing() error {
+	mg.Deps(artefactDirectoryExists)
+
+	var sbErr strings.Builder
+
+	_, err := sh.Exec(nil, nil, &sbErr, "go", "build", "-C", "modules/2_mn_raw_output_processing/", "-o", "../../artefacts/"+outputProcessBin)
+	if err != nil {
+		fmt.Println(sbErr.String())
+	}
 	return err
 }
 
-// BuildCoordinator generates the coordinator binary and sits it in ./artefacts/
-func BuildCoordinator() error {
-	mg.Deps(artefactDirectoryExists)
-	_, err := sh.Exec(nil, nil, nil, "go", "build", "-o", "artefacts/coordinator", "./coordinator")
+//#endregion module building
+
+// Build builds all required files and containers.
+func Build() {
+	mg.Deps(DockerizeIV, BuildCoordinator, BuildSpawnTopo, BuildOutputProcessing)
+}
+
+// Clean deletes the build directory and everything in it.
+func Clean() {
+	sh.Rm(buildDir)
+}
+
+//#region helper functions
+
+// ensures Docker is in path
+func dockerInPath() error {
+	_, err := exec.LookPath("docker")
 	return err
 }
 
@@ -44,7 +87,4 @@ func artefactDirectoryExists() error {
 	return nil
 }
 
-// Build builds all required files and containers.
-func Build() {
-	mg.Deps(DockerizeIV, BuildCoordinator)
-}
+//#endregion helper functions
