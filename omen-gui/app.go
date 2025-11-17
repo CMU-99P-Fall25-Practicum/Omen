65883fdafadd23681c00a036ec8344e328c33c62
R Landau <rflandau@pm.me>
2025-11-17 11:36:02 -0800

    store aps and stas separately

--- omen-gui/app.go
+++ omen-gui/app.go
@@ -3,18 +3,27 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"maps"
 	"os"
+	"slices"
 
 	"github.com/rs/zerolog"
 )
 
 const outPath string = "in.json"
 
-// App struct
+// App is the driver application itself.
+// Some of the components of Input are stored in other fields for easier processing.
+// Input is fully composed and marshaled in GenerateJSON.
 type App struct {
-	ctx    context.Context
-	log    zerolog.Logger
+	ctx context.Context
+	log zerolog.Logger
+
+	// input components
+
 	values Input
+	aps    map[string]AP  // ap name -> ap info
+	sta    map[string]Sta // station name -> station info
 }
 
 // NewApp creates a new App application struct
@@ -26,7 +35,12 @@ func NewApp() (*App, error) {
 		Timestamp().
 		Caller().
 		Logger().Level(zerolog.DebugLevel)
-	return &App{log: l}, nil
+	return &App{
+		log: l,
+
+		aps: map[string]AP{},
+		sta: map[string]Sta{},
+	}, nil
 }
 
 // startup is called when the app starts. The context is saved
@@ -37,12 +51,25 @@ func (a *App) startup(ctx context.Context) {
 
 // AddAP attach an access point to the the input file.
 func (a *App) AddAP(ap AP) {
-	a.log.Info().Str("id", ap.ID).Msg("added access point")
-	a.values.Topo.Aps = append(a.values.Topo.Aps, ap)
+	// check if we are adding or editing
+	_, found := a.aps[ap.ID]
+	a.aps[ap.ID] = ap
+	if !found { // add
+		a.log.Info().Str("id", ap.ID).Msg("added access point")
+	} else { // edit
+		a.log.Info().Str("id", ap.ID).Msg("updated access point")
+	}
 }
 
 func (a *App) AddSta(sta Sta) {
-	a.values.Topo.Stations = append(a.values.Topo.Stations, sta)
+	// check if we are adding or editing
+	_, found := a.sta[sta.ID]
+	a.sta[sta.ID] = sta
+	if !found { // add
+		a.log.Info().Str("id", sta.ID).Msg("added station")
+	} else { // edit
+		a.log.Info().Str("id", sta.ID).Msg("updated station")
+	}
 }
 
 // GenerateJSON composes an input json from the current input values.
@@ -53,6 +80,10 @@ func (a *App) GenerateJSON() (success bool) {
 		return false
 	}
 	defer f.Close()
+
+	// compose all values into struct
+	a.values.Topo.Aps = slices.Collect(maps.Values(a.aps))
+	a.values.Topo.Stations = slices.Collect(maps.Values(a.sta))
 
 	a.log.Debug().Any("values", a.values).Msg("encoding values...")
 
